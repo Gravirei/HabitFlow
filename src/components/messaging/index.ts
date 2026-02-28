37113135afd01c5603285297badc7cc31ada5b44
Gravirei <gravirei@users.noreply.github.com>
2026-02-28 12:21:18 +0600

    feat(02-28): add realtimeService exports to barrel file

--- src/components/messaging/index.ts
+++ src/components/messaging/index.ts
@@ -12,6 +12,17 @@ export * from './constants'
 // Store
 export { useMessagingStore } from './messagingStore'
 
+// Realtime Service
+export {
+  subscribeToConversation,
+  subscribeToTyping,
+  subscribeToPresence,
+  sendTypingIndicator,
+  unsubscribeFromConversation,
+  unsubscribeAll,
+  getConnectionStatus,
+} from './realtimeService'
+
 // Components (added in later phases)
 // Phase 4: MessagingHub, ConversationScreen, MessageBubble, MessageInputBar
 // Phase 5: HabitShareCard, BadgeShareCard, NudgeMessageCard, ReactionPicker, ReactionRow, TypingIndicator
